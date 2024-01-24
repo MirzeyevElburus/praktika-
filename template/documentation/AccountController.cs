c7cabdf5c1dcf9d1308f2198174b876f765ffb84
MirzeyevElburus <elburusmirzeyev397@gmail.com>
2024-01-24 23:46:17 +0400

    ..

---
+++ template/documentation/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using Landab202.Areas.Admin.ViewModels;
+using Landab202.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Landab202.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _man;
+        private readonly SignInManager<AppUser> _sign;
+        private readonly RoleManager<IdentityRole> _role;
+
+        public AccountController(UserManager<AppUser> man,SignInManager<AppUser> sign,RoleManager<IdentityRole> role)
+        {
+            _man = man;
+            _sign = sign;
+            _role = role;
+        }
+        public IActionResult Registr()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult>Registr(RegistrVM vm)
+        {
+            if(!ModelState.IsValid) return View(vm);
+            AppUser user = new AppUser
+            {
+                Email = vm.Email,
+                Name = vm.Name,
+                Surname = vm.Surname,
+                UserName = vm.UserName,
+            };
+            var result=await _man.CreateAsync(user,vm.Password);
+            if(!result.Succeeded)
+            {
+                foreach(var item in result.Errors)
+                {
+                    ModelState.AddModelError(String.Empty, item.Description);
+                }
+                return View(vm);
+            }
+            await _sign.SignInAsync(user, false);
+            return RedirectToAction("Index", "Home", new {area=""});
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult>Login(LoginVM vm)
+        {
+            if (!ModelState.IsValid) return View(vm);
+            AppUser user = await _man.FindByNameAsync(vm.UserNameOrEmail);
+            if(user == null)
+            {
+                user = await _man.FindByEmailAsync(vm.UserNameOrEmail);
+                if(user == null)
+                {
+                    ModelState.AddModelError(String.Empty, "Username ,Email or Password incorrect");
+                }
+                return View(vm);
+            }
+            var result = await _sign.PasswordSignInAsync(user, vm.Password, vm.IsRemembered, false);
+            if(result.IsLockedOut)
+            {
+                ModelState.AddModelError(String.Empty, "Account incorrect try again ");
+                return View(vm);    
+            }
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(String.Empty, "Username ,Email or Password incorrect");
+                return View(vm);
+            }
+            await _sign.SignInAsync(user, false);
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _sign.SignOutAsync();
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+        public async Task<IActionResult> CreateRole()
+        {
+            await _role.CreateAsync(new IdentityRole
+            {
+                Name="Admin"
+            });
+             return RedirectToAction("Index", "Home", new { area = "" });
+        }
+    }
+}
---
+++ template/documentation/ServiceController.cs
@@ -0,0 +1,106 @@
+﻿using Landab202.DAL;
+using Landab202.Models;
+using Landab202.Utilities.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Landab202.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public ServiceController(AppDbContext context,IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+
+            List<Services> s = _context.Servicess.ToList();
+            return View(s);
+        }
+        public IActionResult Create()
+        {
+
+           
+            return View();
+        }
+        [HttpPost]  
+        public async Task<IActionResult>Create(Services s)
+        {
+            if(!ModelState.IsValid) return View(s);
+            if(s.Photo is not null)
+            {
+                if (!s.Photo.ValidateType("image/"))
+                {
+                    ModelState.AddModelError("Photo", "File tipi uyqun deyil");
+                    return View(s);
+                }
+                if (!s.Photo.ValidateSize(2*1024))
+                {
+                    ModelState.AddModelError("Photo", "File tipi uyqun deyil");
+                    return View(s);
+                }
+            }
+            string filename = await s.Photo.CreateAsync(_env.WebRootPath, "assets", "img", "services");
+            Services services = new Services
+            {
+                Image=filename,
+                Name=s.Name,
+                Description=s.Description,  
+            };
+            await _context.Servicess.AddAsync(services);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult>Update(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Services exist=await _context.Servicess.FirstOrDefaultAsync(s => s.Id == id);   
+            if(exist == null) return NotFound();    
+            return View(exist);
+        }
+        [HttpPost]
+        public  async Task<IActionResult>Update(int id,Services s)
+        {
+            if(!ModelState.IsValid) return View(s);
+            Services exist = await _context.Servicess.FirstOrDefaultAsync(s => s.Id == id);
+            if (exist == null) return NotFound();
+            if (s.Photo is not null)
+            {
+                if (!s.Photo.ValidateType("image/"))
+                {
+                    ModelState.AddModelError("Photo", "File tipi uyqun deyil");
+                    return View(s);
+                }
+                if (!s.Photo.ValidateSize(2 * 1024))
+                {
+                    ModelState.AddModelError("Photo", "File tipi uyqun deyil");
+                    return View(s);
+                }
+                string filename = await s.Photo.CreateAsync(_env.WebRootPath, "assets", "img", "services");
+                exist.Image.DeleteFile(_env.WebRootPath, "assets", "img", "services");
+                exist.Image = filename;
+            }
+            exist.Name = s.Name;
+            exist.Description = s.Description;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult>Delete(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Services exist = await _context.Servicess.FirstOrDefaultAsync(s => s.Id == id);
+            if (exist == null) return NotFound();
+            exist.Image.DeleteFile(_env.WebRootPath, "assets", "img", "services");
+            _context.Remove(exist);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
